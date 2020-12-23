88ea77f229dcf105d5e54b6d8e2c9e218d64f86c
Terry Liao <terry.liao@nogle.com>
2020-12-23 17:02:05 +0800

    add services i18n

--- src/locale/zh-TW/index.js
+++ src/locale/zh-TW/index.js
@@ -1,11 +1,13 @@
 import hero from "locale/zh-TW/hero.json";
 import navbar from "locale/zh-TW/navbar.json";
 import info from "locale/zh-TW/info.json";
+import services from "locale/zh-TW/services.json";
 
 const zhTwTranslationObject = {
   ...hero,
   ...navbar,
   ...info,
+  ...services,
 };
 
 export default zhTwTranslationObject;
