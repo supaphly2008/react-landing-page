0ef8835b390d42c4350c26ecb0dbef21ce6c0249
Terry Liao <terry.liao@nogle.com>
2020-12-24 15:05:16 +0800

    set sidebar opacity to 95%

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -17,7 +17,7 @@ const SidebarContainer = styled.aside`
   align-items: center;
   left: 0;
   transition: 0.3s ease-in-out;
-  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  opacity: ${({ isOpen }) => (isOpen ? "95%" : "0")};
   top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
 `;
 
